723100534927d871d7032738f36aa98447db9805
Phate1989 <goldstein.ryan@gmail.com>
2020-09-10 19:04:22 -0400

    both CSV Pre-Compare

--- Main.py
+++ Main.py
@@ -1,21 +1,32 @@
 import csv
 
+Bundle = []
+
+
+with open('AOIT_Internship_Excel.csv', 'r') as file:
+    SKUList = csv.DictReader(file)
+    line_count = 0
+    SKUs = []
+
+    for sku in SKUList:
+        SKUs.append(sku)
+    #    print(row)
+
 
 with open('Bundle_Spreadsheet.csv', 'r') as file:
     reader = csv.DictReader(file)
     line_count = 0
     List = []
+
     for row in reader:
         List.append(row)
-    #    print(row)
-
-
-# print()
-    # print(list)
-    # print()
-    # print(list[0][0])
+        #print(row)
 
 for lines in List:
-    print(lines['Office 365 E3'])
+    Bundle.append(lines['Office 365 E3 Corporate'])
 
+for subSKU in Bundle:
+   
 
+    # for sub in Bundle:
+    # if sub in
